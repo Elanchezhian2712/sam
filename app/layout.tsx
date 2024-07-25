a7383cbe62a0b8731f4a0ad73b1cbc981d3aa32e
Elanchezhian2712 <elanchezhian2712.m@gmail.com>
2024-07-25 08:37:45 +0530

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,19 +7,60 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sam's Portfolio",
-  description: "Modern & Minimal Portfolio",
+  title: "Sam's Portfolio - UI/UX Designer",
+  description:
+    "Discover Sam's award-winning UI/UX designs and learn how they can transform your digital experiences. Ranked top in Google search.",
+  keywords: [
+    "Sam's Portfolio",
+    "UI/UX Designer",
+    "UI Design",
+    "UX Design",
+    "Top UI/UX Designer",
+    "Digital Experiences",
+  ],
+  openGraph: {
+    title: "Sam's Portfolio - UI/UX Designer",
+    description:
+      "Discover Sam's award-winning UI/UX designs and learn how they can transform your digital experiences. Ranked top in Google search.",
+    siteName: "Sam's Portfolio",
+    images: [
+      {
+        url: "/sam-profile.png",
+        width: 1200,
+        height: 630,
+        alt: "Sam's Portfolio - UI/UX Designer",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    icon: "/sam-profile.ico",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/sam-profile.png" sizes="any" />
+        <link rel="icon" href="/sam-profile.ico" sizes="any" />
+        <meta name="robots" content="index, follow" />
+        {/* <link rel="canonical" href="https://samj.vercel.app" /> */}
       </head>
       <body className={inter.className}>
         <ThemeProvider
